1f9406914b8024e122993654c5f99944884cfd62
mmuhunn <dnwn7359@naver.com>
2024-02-21 00:38:06 +0900

    P1874 스택으로 수열만들기

---
+++ Team1/1-Data Structure/Baekjoon/T1 대훈/P11286_절댓값_힙_구현하기.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class P11286_절댓값_힙_구현하기 {
+}
---
+++ Team1/1-Data Structure/Baekjoon/T1 대훈/P1874_스택으로_수열만들기.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class P1874_스택으로_수열만들기 {
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+            int[] A = new int[N];
+
+            for(int i = 0; i < N; ++i) {
+                A[i] = sc.nextInt();
+            }
+
+            Stack<Integer> stack = new Stack();
+            StringBuffer bf = new StringBuffer();
+            boolean result = true;
+            int num = 1;
+
+            for(int i = 0; i < N; ++i) {
+                if (num > A[i]) {
+                    int n = (Integer)stack.pop();
+                    if (n != A[i]) {
+                        System.out.println("NO");
+                        result = false;
+                        break;
+                    }
+
+                    bf.append("-\n");
+                } else {
+                    while(num <= A[i]) {
+                        bf.append("+\n");
+                        stack.push(num++);
+                    }
+
+                    bf.append("-\n");
+                    stack.pop();
+                }
+            }
+
+            if (result) {
+                System.out.println(bf);
+            }
+
+        }
+}
