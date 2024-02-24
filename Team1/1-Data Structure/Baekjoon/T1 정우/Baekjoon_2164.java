8daa64acd7830949f30270fbf5e8652e9c137142
Jungwoo <104622515+yeomjungwoo@users.noreply.github.com>
2024-02-24 05:43:17 +0000

    Solution_Jungwoo_Baekjoon_2164

---
+++ Team1/1-Data Structure/Baekjoon/T1 정우/Baekjoon_2164.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Baekjoon_2164 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        Queue<Integer> q = new LinkedList<>();
+        int N = in.nextInt();
+
+            for(int i = 1; i <= N; i++) {
+                q.offer(i);
+            } // 1부터 N까지 차례대로 큐에 넣기
+
+
+            while(q.size() > 1) {
+                q.poll();	// 맨 앞 숫자 버리기
+                q.offer(q.poll());	// 버렸던 맨 원소 맨 뒤로 삽입하기
+            }
+
+        System.out.println(q.poll());	// 마지막 남은 숫자 출력
+    }
+}
