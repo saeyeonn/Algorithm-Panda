9b1403891ec584302d2c73f0ba00f32b514b05e2
saeyeonn <sy01lim@naver.com>
2024-03-14 14:54:29 +0900

    Setion 6 graph - BOJ 1707, 2252

--- T1-새연/6-Graph/BOJ_1707_이분_그래프.java
+++ T1-새연/6-Graph/BOJ_1707_이분_그래프.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class P1707_이분_그래프 {
+public class BOJ_1707_이분_그래프 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int K = sc.nextInt();
