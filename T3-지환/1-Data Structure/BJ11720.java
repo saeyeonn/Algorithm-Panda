5ddbe83496d1a32f7c84ab3da14398fcf0712022
Jihwan Kim <kimjih1130@gmail.com>
2024-03-21 03:37:04 +0900

    BOJ11720

---
+++ T3-지환/1-Data Structure/BJ11720.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BJ11720 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String m = scanner.next();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum+= m.charAt(i)-48;
+        }
+        System.out.println(sum);
+    }
+}
