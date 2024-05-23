56c71f32f15cd18409ce0d0f3ab98c2d7ea1b3a5
mmuhunn <dnwn7359@naver.com>
2024-05-23 16:03:46 +0900

    백준1929 소수 구하기

--- T1-대훈/5-Number Theory/BOJ1929_소수_구하기.java
+++ T1-대훈/5-Number Theory/BOJ1929_소수_구하기.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class BOJ1929_소수_구하기 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1929_소수_구하기 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            for(int i=M; i<=N; i++){
+                if(judgeDecimal(i)) {
+                    System.out.println(i);
+                }
+            }
+        }
+        public static boolean judgeDecimal(int target){
+            if (target <= 1) {
+                return false; // 1은 소수가 아님
+            }
+            if (target == 2) {
+                return true; // 소수인 2를 따로 연산
+            }
+            if (target % 2 == 0) {
+                return false; // 짝수 제외
+            }
+            for (int i = 3; i <= Math.sqrt(target); i += 2) { // 약수는 항상 제곱근 이하에 존재함
+                if (target % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
 }
